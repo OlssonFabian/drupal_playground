7f5818b53dafbd124becc5ecc35ae27730ac496a
Fabian Olsson <fabtasticdesign@live.se>
2019-04-24 15:14:32 +0200

    WIP debug and working with modules functionality

---
+++ custom/drupal_title_changer_controller/src/Controller/TitleController.php
@@ -0,0 +1,36 @@
+<?php
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+class ExampleForm extends FormBase {
+  public function getFormId() {
+
+    // Unique ID of the form.
+    return 'example_form';
+  }
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Create a $form API array.
+    $form['phone_number'] = array(
+      '#type' => 'tel',
+      '#title' => $this
+        ->t('Your phone number'),
+    );
+    $form['save'] = array(
+      '#type' => 'submit',
+      '#value' => $this
+        ->t('Save'),
+    );
+    return $form;
+  }
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    // Validate submitted form data.
+  }
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Handle submitted form data.
+  }
+
+}
